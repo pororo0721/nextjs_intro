f86ea2e2b6294be11e4efb620ae5b8f13cab5aae
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-02-03 15:10:29 +0000

    Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
+const { redirect } = require("next/dist/server/api-utils");
+
+const API_KEY = process.env.API_KEY;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirect() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+
+},
+    ];
+  },
+};
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Seo from "../components/Seo";
+
+export default function About() {
+    return (
+        <div>
+    <Seo title="About" />
+            <h1>About</h1>
+            <p>This is the about page</p>
+        </div>
+    );
+}
