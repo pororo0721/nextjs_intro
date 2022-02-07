cc331e494f0f6bf261a76c2f0935540766361868
pororo0721 <79802132+pororo0721@users.noreply.github.com>
2022-02-07 16:09:51 +0000

    Movie Detail and Catch All

--- next.config.js
+++ next.config.js
@@ -20,6 +20,10 @@ module.exports = {
         destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
 
 },
+{
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+}
     ];
   },
 };
